9a5a2342d3872e38c68a17482580d47f5e07bcdd
hannes2709 <hannes.gubler@epfl.ch>
2023-02-22 16:09:27 +0100

    asdf

--- Project-1/test.R
+++ Project-1/test.R
@@ -8,8 +8,17 @@ rbilnorm <- function(N, mu1, mu2, sigma1, sigma2, tau){
 }
 
 #pdf from the Gaussian mixture model
-dbilnorm <- function(x, mu1, mu2, sigma1, sigma2, tau){
+dbilnorm2 <- function(x, mu1, mu2, sigma1, sigma2, tau){
   y <- (1-tau)*dlnorm(x,mu1,sigma1) + tau*dlnorm(x,mu2,sigma2)
+  return(y)
+}
+
+dlnorm_test = function(x, mu, sigma) {
+  y = 1 / (x * sigma * sqrt(2 * pi)) * exp(-(log(x) - mu)^2 / (2 * sigma^2))
+  return(y)
+}
+dbilnorm <- function(x, mu1, mu2, sigma1, sigma2, tau){
+  y <- (1-tau)*dlnorm_test(x,mu1,sigma1) + tau*dlnorm_test(x,mu2,sigma2)
   return(y)
 }
 
@@ -20,35 +29,74 @@ EM = function(x, mu1, mu2, sigma1, sigma2, tau) {
   lnew = 10
   while (abs(lold-lnew) > 10^-2) {
     #calculate log likelihood for convergence criteria
-    lold = sum(log((1 - tau)*dlnorm(x, mean = mu1, sd = sqrt(sigma1)) + tau * dlnorm(x, mean = mu2, sd = sqrt(sigma2))))
-    p = dlnorm(x, mean = mu2, sd = sqrt(sigma2)) * tau / dbilnorm(x, mu1, mu2, sqrt(sigma1), sqrt(sigma2), tau)
+    lold = sum(log((1 - tau)*dlnorm(x, mean = mu1, sd = sigma1) + tau * dlnorm(x, mean = mu2, sd = sigma2)))
+    p = dlnorm(x, mean = mu2, sd = sigma2) * tau / dbilnorm(x, mu1, mu2, sigma1, sigma2, tau)
     #update the parameters according to ML estimator of slide 15, week 7
     tau = 1/n * sum(p)
     mu1 = sum(log(x) * (1 - p)) / sum(1 - p)
     mu2 = sum(log(x) * p) / sum(p)
-    sigma1 = sum((1 - p) * ((log(x) - mu1)^2)) / sum(1 - p)
-    sigma2 = sum(p * ((log(x) - mu2)^2)) / sum(p)
+    sigma1 = sqrt(sum((1 - p) * ((log(x) - mu1)^2)) / sum(1 - p))
+    sigma2 = sqrt(sum(p * ((log(x) - mu2)^2)) / sum(p))
     #calculate log likelihood for convergence criteria
-    lnew = sum(log((1 - tau)*dlnorm(x, mean = mu1, sd = sqrt(sigma1)) + tau * dlnorm(x, mean = mu2, sd = sqrt(sigma2))))
+    lnew = sum(log((1 - tau)*dlnorm(x, mean = mu1, sd = sigma1) + tau * dlnorm(x, mean = mu2, sd = sigma2)))
   }
   return(c(mu1, mu2, sigma1, sigma2, tau))
 }
 
 data = read.csv(file = "Project-1/1_snow_particles.csv")
-#Check histogram of the data
-hist(data$retained....)
+num_inbin = length(data$X)
+num_part = data$particles.detected[1]
+for (i in 1:length(data$X)) {
+  num_inbin[i] = round(data$retained....[i] / 100 * num_part)
+}
 #jittering the data
-n = length(data$X)
-noise = runif(n, min = 0, max = 0.05)
-jittered_data = data$startpoint + noise
-hist(jittered_data, breaks = 20)
-testdata = rbilnorm(10000, 1, 5, 1, 1, 0.6)
+n = sum(num_inbin)
+counter = 0
+jittered_data = rep(0, n)
+data$endpoint[52] = 3.25
+for (i in 1:length(num_inbin)) {
+  interval_length = data$endpoint[i] - data$startpoint[i]
+  for(j in 1:num_inbin[i]) {
+    jittered_data[j + counter] = data$startpoint[i] + 
+      runif(1, min = 0, max = interval_length)
+  }
+  counter = counter + num_inbin[i]
+}
+breaks = seq(from = 0, to = 3, by = 0.01)
+hist(jittered_data, breaks=1000)
+#testdata = rbilnorm(10000, 1, 5, 1, 1, 0.6)
 #define starting parameters of the bilognormal model
-mu1 <- 1
-mu2 <- 3
-sigma1 <- 0.5
-sigma2 <- 1
-tau <- 0.5
-estim = EM(testdata, mu1, mu2, sigma1, sigma2, tau)
+mu1 <- -3
+mu2 <- -0.5
+sigma1 <- 0.3
+sigma2 <- 0.4
+tau <- 0.7
+#estim = EM(testdata, mu1, mu2, sigma1, sigma2, tau)
+#print(estim)
+estim = EM(jittered_data, mu1, mu2, sigma1, sigma2, tau)
 print(estim)
+
+
+#optimization 
+#write log likelihood function of binned data
+cdfbilognorm = function(x, mu1, mu2, sigma1, sigma2, tau) {
+  y <- (1 - tau) * plnorm(x, mu1, sigma1) + tau * plnorm(x, mu2, sigma2)
+  return(y)
+}
+
+negloglikelihood = function(par) {
+  logli = 1
+  mu1 = par[1]
+  mu2 = par[2]
+  sigma1 = par[3]
+  sigma2 = par[4]
+  tau = par[5]
+  for (i in 1:length(data$X)) {
+    logli = logli * (cdfbilognorm(data$endpoint[i], mu1, mu2, sigma1, sigma2, tau) - 
+                       cdfbilognorm(data$startpoint[i], mu1, mu2, sigma1, sigma2, tau)) ^ (num_inbin[i])
+  }
+  return(-log(logli))
+}
+out = optim(estim, negloglikelihood)
+
 
