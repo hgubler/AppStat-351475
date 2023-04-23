2e8c134bd0083b1c33429ba952f4cab459830c14
hannes2709 <hannes.gubler@epfl.ch>
2023-04-23 17:04:30 +0200

    change parametric bootstrap

--- Project-4/bootstrap_script.R
+++ Project-4/bootstrap_script.R
@@ -1,15 +1,14 @@
+library(lme4)
 load("Project-4/Data/bootstrap_data.RData")
 # create full model
 mixed_m1 = lmer(dvote ~ . 
                 - evotes 
                 - region 
                 - state_ideology 
-                - dvote_poll 
-                - dapprove_inc
-                - dapprove_presinc 
-                - gnp_growth_inc 
                 - ideology_comp 
-                + (1|year/region), 
+                - year
+                + (1|year:region)
+                + (1|year), 
                 REML = FALSE,
                 data = data)
 # create model without random intercept for year
@@ -17,18 +16,15 @@ mixed_m0 = lmer(dvote ~ .
                 - evotes 
                 - region 
                 - state_ideology 
-                - dvote_poll 
-                - dapprove_inc
-                - dapprove_presinc 
-                - gnp_growth_inc 
                 - ideology_comp 
+                - year
                 + (1|year:region), 
                 REML = FALSE,
                 data = data)
 n = 1000 # number of bootstraps
 #calculate likelihood ratio statistic
 lrstat = as.numeric(2*(logLik(mixed_m1)-logLik(mixed_m0))) 
-lrstats = rep(0,1000) # to store the bootstrapped results
+lrstats = rep(0,n) # to store the bootstrapped results
 set.seed(1) # for reproducability
 for(i in 1:n) {
   # create bootstrap dataset with simulated response
@@ -36,27 +32,22 @@ for(i in 1:n) {
   data_boot$dvote = unlist(simulate(mixed_m0))
   # fit models with simulated response
   mixed_m0_boot = lmer(dvote ~ . 
-                       - evotes  
+                       - evotes 
                        - region 
                        - state_ideology 
-                       - dvote_poll 
-                       - dapprove_inc
-                       - dapprove_presinc 
-                       - gnp_growth_inc 
                        - ideology_comp 
+                       - year
                        + (1|year:region), 
                        REML = FALSE,
                        data = data_boot)  
   mixed_m1_boot = lmer(dvote ~ . 
-                       - evotes  
+                       - evotes 
                        - region 
                        - state_ideology 
-                       - dvote_poll 
-                       - dapprove_inc
-                       - dapprove_presinc 
-                       - gnp_growth_inc 
                        - ideology_comp 
-                       + (1|year/region), 
+                       - year
+                       + (1|year:region)
+                       + (1|year), 
                        REML = FALSE,
                        data = data_boot)  
   # calculate likelihood ratio statistic
@@ -65,6 +56,3 @@ for(i in 1:n) {
 # estimate p-value
 p = mean(lrstats > lrstat)
 print(p)
-# export the p_value as a dataframe
-# p_value = as.data.frame(p)
-# save(p_value, file = "p_value.RData")
